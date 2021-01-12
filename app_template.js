e402a2e0ff606e3ff34bec8818652dc6cab535ef
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-01-12 13:57:19 +0700

    I'm sorry to disappoint you T-T

--- app_template.js
+++ app_template.js
@@ -1,1 +1,13 @@
 //your code here!
+document.addEventListener('DOMContentLaoaded',function () {
+    var numId = document.querySelector('#input-number').onkeyup = function () {
+        console.log("Etas")
+    }
+    
+
+    document.querySelector('#button-compute').onclick = function() {
+        numId = document.querySelector('#input-number')
+        console.log(numId)
+        console.log("test   ")
+    }
+})
